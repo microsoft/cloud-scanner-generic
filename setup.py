b2f96ea9abe8eb398eb1811690247ecfc2cc0e11
Wallace Breza <wbreza@siriusiqmobile.com>
2018-10-15 16:21:36 -0700

    Updated readme and setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='cloud_scanner_generic',
-      version='0.1',
+      version='0.0.1',
       description='Generic utilities for scanning cloud resources',
       url='',
       author='Tanner Barlow',
