0764dca60d983eadcd425902429ba019e1f1b447
Tanner Barlow <tanner.barlow12@gmail.com>
2018-10-19 11:41:02 -0700

    Add version module (#9)

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
+exec(open('cloud_scanner_generic/version.py').read())
 setup(name='cloud_scanner_generic',
-      version='0.0.1',
+      version=__version__,
       description='Generic utilities for scanning cloud resources',
       url='https://microsoft.github.io/cloud-scanner-generic',
       author='Microsoft',
